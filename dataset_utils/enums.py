3b341fcf32d0b6eca0232244070ac9ac236f6139
Shiv Murthy <sm2678@rit.edu>
2023-11-30 13:27:12 -0500

    added [QUESTION_TYPE] spl token

--- dataset_utils/enums.py
+++ dataset_utils/enums.py
@@ -44,6 +44,7 @@ class Enums:
 
     QUESTION_SPECIAL_TOKEN = "[Question]"
     CONTEXT_SPECIAL_TOKEN = "[CONTEXT]"
+    QUESTION_TYPE_SPECIAL_TOKEN = "[QUESTION_TYPE]"
 
     MAX_LEN = 30 # estimated through EDA.  Longest Annotation in Training is 26; Longest Annotation in Validation is 23
     ANSWERS_PER_QUESTION = 10
